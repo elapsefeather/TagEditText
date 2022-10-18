9bf52b8841eef577c8a4876a94b5b595cb5f05d0
羿婷 <e25824564@gmail.com>
2022-10-18 13:27:58 +0800

    [Add] exp

--- app/src/main/java/com/elapsefeather/tagedittext/MainActivity.java
+++ app/src/main/java/com/elapsefeather/tagedittext/MainActivity.java
@@ -4,11 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.elapsefeather.tagedittext.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    List<TagData> tagList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initTag();
+        initView();
+    }
+
+    private void initTag() {
+        tagList.clear();
+        for (int i = 1; i <= 20; i++) {
+            tagList.add(new TagData("name" + i));
+        }
+    }
+
+    private void initView() {
+        binding.send.setOnClickListener(v -> {
+            if (binding.tagEdit.getText() == null || binding.tagEdit.getText().toString().equals(""))
+                return;
+            binding.tagText.setText(binding.tagEdit.getText().toString());
+            binding.tagEdit.setText("");
+        });
+
+        binding.tagText.setTags(tagList);
+        binding.tagText.setTagColor(getColor(R.color.purple_200));
+        binding.tagEdit.setTags(tagList);
+        binding.tagEdit.setTagColor(getColor(R.color.teal_700));
+        binding.tagEdit.setAdapter(new TagAdapter(this, R.layout.item_tag, tagList));
     }
 }
